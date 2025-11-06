78797f077bb3871e7d854b79643618570e315345
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-06 17:03:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 mcsmanager_install_path="/opt/mcsmanager"
 mcsmanager_download_addr="https://github.com/thororen1234/MCSManager/releases/latest/download/mcsmanager_linux_release.tar.gz"
 package_name="mcsmanager_linux_release.tar.gz"
-node="v20.12.2"
 arch=$(uname -m)
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -43,42 +42,6 @@ Red_Error() {
   exit 1
 }
 
-Install_Node() {
-  if [[ -f "$node_install_path"/bin/node ]] && [[ "$("$node_install_path"/bin/node -v)" == "$node" ]]; then
-    echo_green "Node.js version is up-to-date, skipping installation."
-    return
-  fi
-
-  echo_cyan "[+] Install Node.JS environment..."
-
-  rm -irf "$node_install_path"
-
-  cd /opt || Red_Error "[x] Failed to enter /opt"
-
-  rm -rf "node-$node-linux-$arch.tar.gz"
-
-  wget "https://nodejs.org/dist/$node/node-$node-linux-$arch.tar.gz" || Red_Error "[x] Failed to download node release"
-
-  tar -zxf "node-$node-linux-$arch.tar.gz" || Red_Error "[x] Failed to untar node"
-
-  rm -rf "node-$node-linux-$arch.tar.gz"
-
-  if [[ -f "$node_install_path"/bin/node ]] && [[ "$("$node_install_path"/bin/node -v)" == "$node" ]]; then
-    echo_green "Success"
-  else
-    Red_Error "[x] Node installation failed!"
-  fi
-
-  echo
-  echo_yellow "=============== Node.JS Version ==============="
-  echo_yellow " node: $("$node_install_path"/bin/node -v)"
-  echo_yellow " npm: v$(env "$node_install_path"/bin/node "$node_install_path"/bin/npm -v)"
-  echo_yellow "=============== Node.JS Version ==============="
-  echo
-
-  sleep 3
-}
-
 Install_MCSManager() {
   echo_cyan "[+] Install MCSManager..."
 
@@ -109,13 +72,13 @@ Install_MCSManager() {
   cd "${mcsmanager_install_path}/daemon" || Red_Error "[x] Failed to enter ${mcsmanager_install_path}/daemon"
 
   echo_cyan "[+] Install MCSManager-Daemon dependencies..."
-  env "$node_install_path"/bin/node "$node_install_path"/bin/npm install --production --no-fund --no-audit &>/dev/null || Red_Error "[x] Failed to npm install in ${mcsmanager_install_path}/daemon"
+  env "npm install --production --no-fund --no-audit &>/dev/null || Red_Error "[x] Failed to npm install in ${mcsmanager_install_path}/daemon"
 
   # echo "[←] cd .."
   cd "${mcsmanager_install_path}/web" || Red_Error "[x] Failed to enter ${mcsmanager_install_path}/web"
 
   echo_cyan "[+] Install MCSManager-Web dependencies..."
-  env "$node_install_path"/bin/node "$node_install_path"/bin/npm install --production --no-fund --no-audit &>/dev/null || Red_Error "[x] Failed to npm install in ${mcsmanager_install_path}/web"
+  env "npm install --production --no-fund --no-audit &>/dev/null || Red_Error "[x] Failed to npm install in ${mcsmanager_install_path}/web"
 
   echo
   echo_yellow "=============== MCSManager ==============="
@@ -130,119 +93,5 @@ Install_MCSManager() {
   sleep 3
 }
 
-Create_Service() {
-  echo_cyan "[+] Create MCSManager service..."
-
-  echo "[Unit]
-Description=MCSManager-Daemon
-
-[Service]
-WorkingDirectory=${mcsmanager_install_path}/daemon
-ExecStart=${node_install_path}/bin/node app.js
-ExecReload=/bin/kill -s QUIT \$MAINPID
-ExecStop=/bin/kill -s QUIT \$MAINPID
-Environment=\"PATH=${PATH}\"
-
-[Install]
-WantedBy=multi-user.target
-" >/etc/systemd/system/mcsm-daemon.service
-
-  echo "[Unit]
-Description=MCSManager-Web
-
-[Service]
-WorkingDirectory=${mcsmanager_install_path}/web
-ExecStart=${node_install_path}/bin/node app.js
-ExecReload=/bin/kill -s QUIT \$MAINPID
-ExecStop=/bin/kill -s QUIT \$MAINPID
-Environment=\"PATH=${PATH}\"
-
-[Install]
-WantedBy=multi-user.target
-" >/etc/systemd/system/mcsm-web.service
-
-  systemctl daemon-reload
-  systemctl enable --now mcsm-{daemon,web}.service
-  echo_green "Registered!"
-
-  sleep 2
-
-  printf "\n\n\n\n"
-
-  echo_yellow "=================================================================="
-  echo_green "Installation is complete! Welcome to the MCSManager!!!"
-  echo_yellow " "
-  echo_cyan_n "HTTP Web Service:        "
-  echo_yellow "http://<Your IP>:23333  (Browser)"
-  echo_cyan_n "Daemon Address:          "
-  echo_yellow "ws://<Your IP>:24444    (Cluster)"
-  echo_red "You must expose ports "
-  echo_yellow "23333"
-  echo_red " and "
-  echo_yellow "24444"
-  echo_red " to use the service properly on the Internet."
-  echo_yellow " "
-  echo_cyan "Usage:"
-  echo_cyan "systemctl start mcsm-{daemon,web}.service"
-  echo_cyan "systemctl stop mcsm-{daemon,web}.service"
-  echo_cyan "systemctl restart mcsm-{daemon,web}.service"
-  echo_yellow " "
-  echo_green "Official Document: https://docs.mcsmanager.com/"
-  echo_yellow "=================================================================="
-}
-
-# Environmental inspection
-if [[ "$arch" == x86_64 ]]; then
-  arch=x64
-  #echo "[-] x64 architecture detected"
-elif [[ $arch == aarch64 ]]; then
-  arch=arm64
-  #echo "[-] 64-bit ARM architecture detected"
-elif [[ $arch == arm ]]; then
-  arch=armv7l
-  #echo "[-] 32-bit ARM architecture detected"
-elif [[ $arch == ppc64le ]]; then
-  arch=ppc64le
-  #echo "[-] IBM POWER architecture detected"
-elif [[ $arch == s390x ]]; then
-  arch=s390x
-  #echo "[-] IBM LinuxONE architecture detected"
-else
-  Red_Error "[x] Sorry, this architecture is not supported yet!\n[x]Please try to install manually: https://github.com/MCSManager/MCSManager#linux"
-fi
-
-# Define the variable Node installation directory
-node_install_path="/opt/node-$node-linux-$arch"
-
-# Check network connection
-echo_cyan "[-] Architecture: $arch"
-
-# Install related software
-echo_cyan_n "[+] Installing dependent software (git, tar, wget)... "
-if [[ -x "$(command -v yum)" ]]; then
-  yum install -y git tar wget
-elif [[ -x "$(command -v apt-get)" ]]; then
-  apt-get install -y git tar wget
-elif [[ -x "$(command -v pacman)" ]]; then
-  pacman -S --noconfirm --needed git tar wget
-elif [[ -x "$(command -v zypper)" ]]; then
-  zypper --non-interactive install git tar wget
-else
-  echo_red "[!] Cannot find your package manager! You may need to install git, tar and wget manually!"
-fi
-
-# Determine whether the relevant software is installed successfully
-if [[ -x "$(command -v git)" && -x "$(command -v tar)" && -x "$(command -v wget)" ]]; then
-  echo_green "Success"
-else
-  Red_Error "[x] Failed to find git, tar and wget, please install them manually!"
-fi
-
-# Install the Node environment
-Install_Node
-
 # Install MCSManager
 Install_MCSManager
-
-# Create MCSManager background service
-Create_Service
